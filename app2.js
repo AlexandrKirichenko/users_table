81994489290a6436c5dd650fdec3f76e518129c2
AlexandrKirichenko <52669128+AlexandrKirichenko@users.noreply.github.com>
2020-05-03 21:33:35 +0300

    Update app2.js

--- app2.js
+++ app2.js
@@ -1,1 +1,7 @@
-document.querySelectorAll('tr > td:last-child')
+ const sum = [...document.querySelectorAll('tr > td:last-child')].map(el => Number(el.innerHTML)).reduce((acc, el) => acc+el,0) //превратили в массив, и суммируем Salary
+ console.log(sum);
+
+ const html = document.createElement('div');
+ html.innerText = sum + '$';
+// document.getElementsByClassName('col-12')[0].appendChild(html);
+document.querySelector('table > tbody > tr:last-child > td:last-child').appendChild(html);
